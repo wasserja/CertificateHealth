b5058a261672378592acb97e2049bd710d4807a1
wasserja <wasserja@users.noreply.github.com>
2015-10-16 15:53:54 -0400

    Initial Commit

---
+++ CertificateHealth.psm1
@@ -0,0 +1,8 @@
+﻿# Source all ps1 scripts in current directory.
+Get-ChildItem (Join-Path $PSScriptRoot *.ps1) | foreach {. $_.FullName}
+
+# Making Parent Functions Available
+Export-ModuleMember -Function Get-CertificateFile
+Export-ModuleMember -Function Get-CertificateHealth
+Export-ModuleMember -Function Get-UnhealthyCertificate
+Export-ModuleMember -Function Get-UnhealthyCertificateNagios
