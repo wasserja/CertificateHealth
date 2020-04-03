68934efa60167d9799bb8c953e26b3fcd23eaf6f
@wasserja <wasserja@users.noreply.github.com>
2020-04-03 16:35:13 -0400

    1.7

--- Get-SchannelProtocol.ps1
+++ Get-SchannelProtocol.ps1
@@ -10,17 +10,36 @@ Specify the protocol you want to query.
 Specify the communication mode: server/client.
 .EXAMPLE
 Get-SchannelProtocol
+
+Protocol DisabledByDefault Enabled CommunicationMode
+-------- ----------------- ------- -----------------
+SSL2                  True   False Client
+SSL2                  True   False Server
+SSL3                  True   False Client
+SSL3                  True   False Server
+TLS1.0                True   False Client
+TLS1.0                True   False Server
+TLS1.1                True   False Client
+TLS1.1                True   False Server
+TLS1.2               False    True Client
+TLS1.2               False    True Server
+TLS1.3             Not Set Not Set Client
+TLS1.3             Not Set Not Set Server
 .EXAMPLE
-Get-NetCertificate -Protocol TLS1.2 -CommunicationMode Server
+Get-SchannelProtocol -Protocol TLS1.2 -CommunicationMode Server
+
+Protocol DisabledByDefault Enabled CommunicationMode
+-------- ----------------- ------- -----------------
+TLS1.2               False    True Server
 .NOTES
 Created by: Jason Wasser
-Modified: 4/3/2020 01:20:20 PM 
+Modified: 4/3/2020
 #>
 function Get-SchannelProtocol {
     [cmdletbinding()]
     param (
-        [ValidateSet('SSL2', 'SSL3', 'TLS1.0', 'TLS1.1', 'TLS1.2','TLS1.3')]
-        [string[]]$Protocol = ('SSL2', 'SSL3', 'TLS1.0', 'TLS1.1', 'TLS1.2','TLS1.3'),
+        [ValidateSet('SSL2', 'SSL3', 'TLS1.0', 'TLS1.1', 'TLS1.2', 'TLS1.3')]
+        [string[]]$Protocol = ('SSL2', 'SSL3', 'TLS1.0', 'TLS1.1', 'TLS1.2', 'TLS1.3'),
         [ValidateSet('Client', 'Server')]
         [string[]]$CommunicationMode = ('Client', 'Server')
     )
@@ -50,7 +69,6 @@ function Get-SchannelProtocol {
                     Write-Verbose "Protocol Status Registry Value for $StatusCheck is not present."
                     $ProtocolStatus = 'Not Set'
                 }
-        
             }
             catch [System.Exception] {
                 switch ($_.Exception.GetType().FullName) {
@@ -86,6 +104,9 @@ function Get-SchannelProtocol {
                     }
                     'TLS1.2' {
                         $ProtocolName = 'TLS 1.2'
+                    }
+                    'TLS1.3' {
+                        $ProtocolName = 'TLS 1.3'
                     }
                 }
 
